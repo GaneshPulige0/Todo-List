079eef35600978ca4feae0d61ebd2cadb4a8e6b6
pulige ganesh <ganeshpulige@gmail.com>
2025-10-11 03:08:31 +0530

    todo container add

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 let todoItemsContainer = document.getElementById("todoItemsContainer");
 
+
+// creating list item. adding to todoItemsContainer
+let todoElement = document.createElement("li")
+todoElement.classList.add("todo-items-container","d-flex","flex-row")
+todoItemsContainer.appendChild(todoElement)
+// -----------------------------------------------------------------------
+
+
+// creating checkbox and adding to todoElement
 let inputElement = document.createElement("input")
 inputElement.type = "Checkbox"
 inputElement.classList.add("checkbox-input")
-todoItemsContainer.appendChild(inputElement)
+todoElement.appendChild(inputElement)
+// /----------------------------------------------------------------
+
+
+// creating div and label and adding to todoElement
+labelElementContainer = document.createElement("div")
+labelElementContainer.classList.add("d-flex","flex-row","label-container")
+todoElement.appendChild(labelElementContainer)
+
+
 
