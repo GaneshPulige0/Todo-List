f0ebf4b409cc249d02d7bd57a227a3efcda46d61
pulige ganesh <ganeshpulige@gmail.com>
2025-10-11 01:15:03 +0530

    added checkbox in js

---
+++ index.js
@@ -0,0 +1,7 @@
+let todoItemsContainer = document.getElementById("todoItemsContainer");
+
+let inputElement = document.createElement("input")
+inputElement.type = "Checkbox"
+inputElement.classList.add("checkbox-input")
+todoItemsContainer.appendChild(inputElement)
+
