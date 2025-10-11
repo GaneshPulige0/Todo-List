76f2f1cf58f3f28735273c40aaf854764f1c9acf
pulige ganesh <ganeshpulige@gmail.com>
2025-10-11 13:18:17 +0530

    todo added label text and icon

--- index.js
+++ index.js
@@ -16,10 +16,24 @@ todoElement.appendChild(inputElement)
 // /----------------------------------------------------------------
 
 
-// creating div and label and adding to todoElement
+// creating div and adding to todoElement
 labelElementContainer = document.createElement("div")
 labelElementContainer.classList.add("d-flex","flex-row","label-container")
 todoElement.appendChild(labelElementContainer)
 
+// creating a label element adding to and labelelementcontainer
+labelElement = document.createElement("label")
+labelElement.textContent = "Learn HTML"
+labelElement.classList.add("checkbox-label")
+labelElementContainer.appendChild(labelElement)
 
 
+
+// creating a div for deleting icon. and append to labelElement
+let deleteIconContainer = document.createElement("div") 
+deleteIconContainer.classList.add("delete-icon-container","d-flex","flex-row")
+labelElement.appendChild(deleteIconContainer)
+
+let iconElement = document.createElement("i");
+iconElement.classList.add("far", "fa-trash-alt", "delete-icon")
+deleteIconContainer.appendChild(iconElement)
